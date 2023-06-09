f31fbc8116a6d27e05ab388690cf2072c08e658c
Hasi-b <hasib.hasan9@gmail.com>
2023-06-09 10:43:45 +0600

    particle added

--- Assets/HyperCasual/Scripts/DetectCollision.cs
+++ Assets/HyperCasual/Scripts/DetectCollision.cs
@@ -7,21 +7,37 @@ public class DetectCollision : MonoBehaviour
 {
     public static event Action OnEnterHex;
     public static event Action OnCollideHex;
+    [SerializeField]
+    GameObject burstBubble;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Hex"))
         {
+            SpawnAndPlayParticleSystem(Color.black);
             OnCollideHex?.Invoke();
             Destroy(this.gameObject);
+           
         }
         else if (collision.CompareTag("Open"))
         {
+            SpawnAndPlayParticleSystem(Color.white);
             OnEnterHex?.Invoke();
             Destroy(this.gameObject);
         }
 
        
+    }
+
+
+    public void SpawnAndPlayParticleSystem(Color color)
+    {
+        GameObject particleObject = Instantiate(burstBubble,transform.position,Quaternion.identity);
+        ParticleSystem particleSystem = particleObject.GetComponent<ParticleSystem>();
+        ParticleSystem.MainModule particleMainModule;
+        particleMainModule = particleSystem.main;
+        particleMainModule.startColor = color;
+        particleSystem.Play();
     }
 }
